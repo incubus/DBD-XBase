b805950c20046185e40452d9861ed1039f838acf
Jan Pazdziora <adelton@fi.muni.cz>
2001-12-07 12:56:42 +0100

    README and INSTALL updated.

--- lib/XBase.pm
+++ lib/XBase.pm
@@ -20,7 +20,7 @@ use XBase::Base;		# will give us general methods
 use vars qw( $VERSION $errstr $CLEARNULLS @ISA );
 
 @ISA = qw( XBase::Base );
-$VERSION = '0.200';
+$VERSION = '0.210';
 $CLEARNULLS = 1;		# Cut off white spaces from ends of char fields
 
 *errstr = \$XBase::Base::errstr;
@@ -1342,7 +1342,7 @@ Thanks a lot.
 
 =head1 VERSION
 
-0.200
+0.210
 
 =head1 AUTHOR
 
